c6d8d1d94e8db57c6500833445a73f2811e0bab6
Nick Simonov <nsimonov@e1r2p16.unit.ua>
2018-03-17 18:26:12 +0200

    --amend

--- includes/malloc.h
+++ includes/malloc.h
@@ -8,7 +8,7 @@
 #define T_SIZE 8
 #define T_PAGE_SIZE (8 * getpagesize())
 #define S_SIZE 32
-#define S_PAGE_SIZE (32 * getpagesize());
+#define S_PAGE_SIZE (32 * getpagesize())
 
 extern pthread_mutex_t mutex;
 
@@ -26,12 +26,6 @@ struct s_block
 	struct s_block *next;
 };
 
-struct t_block
-{
-  	size_t size;
-  	size_t inuse;
-  	struct s_block *next;
-};
 
 struct l_block
 {
@@ -50,7 +44,7 @@ struct block_addr
   	char   *t_s_addr;
   	char   *t_e_addr;
   	char   *s_s_addr;
-  	char   *t_e_addr;
+  	char   *s_e_addr;
   	char   *l_s_addr;
 };
 
@@ -69,6 +63,7 @@ struct ovrl_block
   	struct l_block *l;
 };
 
-extern ovrl_block *g_dma;
+extern struct ovrl_block *g_dma;
 
-#endi
+void init_memory(void);
+#endif
--- src/init_memory.c
+++ src/init_memory.c
@@ -1,29 +1,41 @@
 #include "../includes/malloc.h"
+#include <stdio.h>
 
 struct ovrl_block *g_dma = NULL;
 
-void init_tiny(void)
+static void init_tiny(void)
 {
-  	g_dma->t = (char*)(g_dma + 1);
+  printf("start addr %p\n", (struct t_block*)(g_dma + 1));
+  	g_dma->t = (struct t_block*)(g_dma + 1);
 
-	g_dma->t_s_addr = (char*)(g_dma + 1);
-	g_dma->t_e_addr = (char*)(g_dma + T_PAGE_SIZE + 1);
+	//	write(1, "init success1\n", 30);
+	//g_dma->addr = (struct block_addr*)(g_dma + 1);
+	//	g_dma->addr->t_s_addr = (char *)(g_dma + 1);
+	//g_dma->addr->t_e_addr = (char *)(g_dma + T_PAGE_SIZE + 1);
 
   	g_dma->t->size = 0;
   	g_dma->t->inuse = 0;
 	g_dma->t->next = NULL;
 }
 
-void init_small(void)
+static void init_small(void)
 {
-  	g_dma->s = (char*)(g_dma->t_e_addr + 1);
+  	g_dma->s = (struct s_block*)(g_dma->addr->t_e_addr + 1);
 
-	g_dma->s_s_addr = (char*)(g_dma->s + 1);
-	g_dma->s_e_addr = (char*)(g_dma + S_PAGE_SIZE + 1);
+	g_dma->addr->s_s_addr = (char*)(g_dma->s + 1);
+	g_dma->addr->s_e_addr = (char*)(g_dma + S_PAGE_SIZE + 1);
 
 	g_dma->s->size = 0;
 	g_dma->s->inuse = 0;
 	g_dma->s->next = NULL;
+}
+
+static void init_large(void)
+{
+  	g_dma->l = NULL;
+	g_dma->l->size = 0;
+	g_dma->l->inuse = 0;
+	g_dma->l->next = NULL;
 }
 
 void init_memory(void)
@@ -34,8 +46,9 @@ void init_memory(void)
 		       + (T_PAGE_SIZE * 100)
 		       + (S_PAGE_SIZE * 100),
 		       PROT_READ | PROT_WRITE, MAP_ANON, -1, 0);
-	  init_tiny();
-	  init_small();
-	  inti_large();
-	}
+	  
+	    init_tiny();
+	    //init_small();
+	  }
+	  //init_large();
 }
