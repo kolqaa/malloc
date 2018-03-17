44a80fba044caed007d64272e715ec6dc3f3f72e
Nick Simonov <nsimonov@e1r2p16.unit.ua>
2018-03-17 16:08:21 +0200

    init_mem and initial malloc

--- includes/malloc.h
+++ includes/malloc.h
@@ -1,9 +1,16 @@
 #ifndef MALLOC_H
 #define MALLOC_H
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <pthread.h>
+
 #define T_SIZE 8
 #define T_PAGE_SIZE (8 * getpagesize())
 #define S_SIZE 32
 #define S_PAGE_SIZE (32 * getpagesize());
+
+extern pthread_mutex_t mutex;
 
 struct t_block
 {
@@ -19,6 +26,12 @@ struct s_block
 	struct s_block *next;
 };
 
+struct t_block
+{
+  	size_t size;
+  	size_t inuse;
+  	struct s_block *next;
+};
 
 struct l_block
 {
@@ -27,6 +40,11 @@ struct l_block
   	struct l_block *next;
 };
 
+/**
+ * @block_addr: stroes info about zones addresses for tiny,
+ *              small and large. It include pointer to start
+ *              (x_s_addr) and end (x_e_addr) addres
+ */
 struct block_addr
 {
   	char   *t_s_addr;
@@ -36,13 +54,19 @@ struct block_addr
   	char   *l_s_addr;
 };
 
+/**
+ * @ovrl_block: stores info about memory zone
+ * addr: stores start/end addreses of every zone
+ * t: tiny
+ * s: small
+ * l: large
+ */
 struct ovrl_block
 {
-	/* memory addressed start - end */
   	struct block_addr *addr;
-  	/* memory block */
 	struct s_block *s;
   	struct t_block *t;
+  	struct l_block *l;
 };
 
 extern ovrl_block *g_dma;
--- src/init_memory.c
+++ src/init_memory.c
@@ -2,6 +2,30 @@
 
 struct ovrl_block *g_dma = NULL;
 
+void init_tiny(void)
+{
+  	g_dma->t = (char*)(g_dma + 1);
+
+	g_dma->t_s_addr = (char*)(g_dma + 1);
+	g_dma->t_e_addr = (char*)(g_dma + T_PAGE_SIZE + 1);
+
+  	g_dma->t->size = 0;
+  	g_dma->t->inuse = 0;
+	g_dma->t->next = NULL;
+}
+
+void init_small(void)
+{
+  	g_dma->s = (char*)(g_dma->t_e_addr + 1);
+
+	g_dma->s_s_addr = (char*)(g_dma->s + 1);
+	g_dma->s_e_addr = (char*)(g_dma + S_PAGE_SIZE + 1);
+
+	g_dma->s->size = 0;
+	g_dma->s->inuse = 0;
+	g_dma->s->next = NULL;
+}
+
 void init_memory(void)
 {
   	if (!g_dma)
@@ -12,5 +36,6 @@ void init_memory(void)
 		       PROT_READ | PROT_WRITE, MAP_ANON, -1, 0);
 	  init_tiny();
 	  init_small();
+	  inti_large();
 	}
 }
