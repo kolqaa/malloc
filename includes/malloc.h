f083e0d23cbefe42beb4d88ed5f1e943946a7d42
Mykola Simonov <mykola.simonov@globallogic.com>
2018-04-20 20:39:51 +0300

    update init and header

--- includes/malloc.h
+++ includes/malloc.h
@@ -7,8 +7,15 @@
 
 #define T_SIZE 8
 #define T_PAGE_SIZE (8 * getpagesize())
+#define T_LIMIT (getpagesize() * 100 * T_SIZE)
+
 #define S_SIZE 32
 #define S_PAGE_SIZE (32 * getpagesize())
+#define S_LIMIT (getpagesize() * 100 * S_SIZE)
+
+
+#define MMAP_FLAGS PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE
+#define OFFSET -1
 
 extern pthread_mutex_t mutex;
 
@@ -17,13 +24,15 @@ struct t_block
 	size_t size;
 	size_t inuse;
 	struct t_block *next;
+    struct t_block *blck_limit;
 };
 
-struct s_block 
+struct s_block
 {
 	size_t size;
 	size_t inuse;
 	struct s_block *next;
+    struct s_block *blck_limit;
 };
 
 
@@ -32,6 +41,7 @@ struct l_block
   	size_t size;
   	size_t inuse;
   	struct l_block *next;
+    struct l_block *blck_limit;
 };
 
 /**
@@ -41,11 +51,9 @@ struct l_block
  */
 struct block_addr
 {
-  	char   *t_s_addr;
-  	char   *t_e_addr;
-  	char   *s_s_addr;
-  	char   *s_e_addr;
-  	char   *l_s_addr;
+  	char   *tiny_start;
+  	char   *small_start;
+  	char   *large_start;
 };
 
 /**
@@ -57,10 +65,12 @@ struct block_addr
  */
 struct ovrl_block
 {
-  	struct block_addr *addr;
-	struct s_block *s;
-  	struct t_block *t;
-  	struct l_block *l;
+  	struct t_block *tiny;
+    size_t tiny_limit;
+	struct s_block *small;
+    size_t small_limit;
+  	struct l_block *large;
+  	struct block_addr addr;
 };
 
 extern struct ovrl_block *g_dma;
--- src/init_memory.c
+++ src/init_memory.c
@@ -7,49 +7,64 @@ struct ovrl_block *g_dma = NULL;
 
 static void init_tiny(void)
 {
-  	g_dma->t = (struct t_block *)(g_dma + 1);
+    g_dma->tiny = (struct t_block *)(g_dma + 1);
+    g_dma->tiny->blck_limit = g_dma->tiny + 2;
+    g_dma->tiny_limit = T_LIMIT;
 
-	g_dma->addr = (struct block_addr*)(g_dma + 1);
-	g_dma->addr->t_s_addr = (char *)(g_dma + 1);
-	g_dma->addr->t_e_addr = (char *)(g_dma + T_PAGE_SIZE + 1);
+    g_dma->tiny->next = NULL;
+    g_dma->tiny->size = 0;
+    g_dma->tiny->inuse = 0;
 
-  	g_dma->t->size = 0;
-  	g_dma->t->inuse = 0;
-	g_dma->t->next = NULL;
+    g_dma->addr.tiny_start = (char*)g_dma->tiny;
 }
 
 static void init_small(void)
 {
-  	g_dma->s = (struct s_block*)(g_dma->addr->t_e_addr + 1);
+    g_dma->small = (struct s_block *)((char *)g_dma->tiny + g_dma->tiny_limit);
+    g_dma->small->blck_limit = g_dma->small + 1;
+    g_dma->small_limit = S_LIMIT;
 
-	g_dma->addr->s_s_addr = (char*)(g_dma->s + 1);
-	g_dma->addr->s_e_addr = (char*)(g_dma + S_PAGE_SIZE + 1);
+    g_dma->small->next = NULL;
+    g_dma->small->size = 0;
+    g_dma->small->inuse = 0;
 
-	g_dma->s->size = 0;
-	g_dma->s->inuse = 0;
-	g_dma->s->next = NULL;
+    g_dma->addr.small_start = (char*)g_dma->small;
 }
 
-static void init_large(void)
+
+
+void show_alloc_mem(void)
+{
+    printf("TINY: %p\n", g_dma->tiny);
+    printf("SMALL: %p\n", g_dma->small);
+}
+
+/*static void init_large(void)
 {
   	g_dma->l = NULL;
 	g_dma->l->size = 0;
 	g_dma->l->inuse = 0;
 	g_dma->l->next = NULL;
-}
+    }*/
 
 void init_memory(void)
 {
 	const size_t SIZE = (sizeof(struct ovrl_block) + T_PAGE_SIZE + S_PAGE_SIZE) * 200;
   	if (!g_dma)
 	{
-		if ((g_dma = mmap(NULL, SIZE,
-						  PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
-		{
+		if ((g_dma = mmap(NULL, SIZE, MMAP_FLAGS, OFFSET, 0)) == MAP_FAILED)
 			exit(0);
-		}
 		init_tiny();
-	    //init_small();
-	  }
+        init_small();
+    }
 	  //init_large();
+}
+
+
+int main(void)
+{
+    init_memory();
+    show_alloc_mem();
+
+    return 0;
 }
