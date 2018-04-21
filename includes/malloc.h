3e918ee84f1d0c61577c90f38e96bf9980ba9cab
Nick Simonov <nicksimonovv@gmail.com>
2018-04-21 23:44:30 +0300

    rewrite initalizing + chunck adding

--- includes/malloc.h
+++ includes/malloc.h
@@ -4,27 +4,29 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <pthread.h>
+#include <errno.h>
 
 #define T_SIZE 8
+#define PREALOC_SIZE_TINY (getpagesize() * 100 * T_SIZE)
 #define T_PAGE_SIZE (8 * getpagesize())
 #define T_LIMIT (getpagesize() * 100 * T_SIZE)
 
 #define S_SIZE 32
+#define PREALOC_SIZE_SMALL (getpagesize() * 100 * S_SIZE)
 #define S_PAGE_SIZE (32 * getpagesize())
 #define S_LIMIT (getpagesize() * 100 * S_SIZE)
 
-
+#define HDR_SIZE (sizeof(struct ovrl_block))
 #define MMAP_FLAGS PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE
 #define OFFSET -1
 
-extern pthread_mutex_t mutex;
 
 struct t_block
 {
 	size_t size;
 	size_t inuse;
 	struct t_block *next;
-    struct t_block *blck_limit;
+  	struct t_block *blck_limit;
 };
 
 struct s_block
@@ -32,7 +34,7 @@ struct s_block
 	size_t size;
 	size_t inuse;
 	struct s_block *next;
-    struct s_block *blck_limit;
+  	struct s_block *blck_limit;
 };
 
 
@@ -41,7 +43,7 @@ struct l_block
   	size_t size;
   	size_t inuse;
   	struct l_block *next;
-    struct l_block *blck_limit;
+  	struct l_block *blck_limit;
 };
 
 /**
@@ -66,14 +68,19 @@ struct block_addr
 struct ovrl_block
 {
   	struct t_block *tiny;
-    size_t tiny_limit;
+  	size_t tiny_limit;
 	struct s_block *small;
-    size_t small_limit;
+  	size_t small_limit;
   	struct l_block *large;
   	struct block_addr addr;
+
+  	void *(*get_tiny)(struct t_block* , size_t, size_t);
+  //  	void *(get_small)(struct s_block* , size_t, size_t);
+  //void *(get_large)(struct l_block* , size_t, size_t);
 };
 
 extern struct ovrl_block *g_dma;
+extern pthread_mutex_t mutex;
 
 void init_memory(void);
 #endif
--- src/init_memory.c
+++ src/init_memory.c
@@ -4,12 +4,50 @@
 #include <stdlib.h>
 
 struct ovrl_block *g_dma = NULL;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+enum CHUNK_TYPE {TINY, SMALL};
+
+
+
+#define ADD_CHUNK_TO_DMA(TYPE, TINY_NODE, TMP) ({\
+    TMP = TINY_NODE->next; \
+    TINY_NODE->next = TINY_NODE->blck_limit; \
+    TINY_NODE->next->blck_limit = \
+    (struct t_block*)((char*)tiny_node->next + size); \
+    TINY_NODE->next->next = tmp; \
+    TINY_NODE->next->size = size; \
+})
+
+void *add_tiny(struct t_block *tiny_head, size_t size, size_t max)
+{
+    struct  t_block *tiny_node;
+    struct  t_block *tmp;
+    size += sizeof(struct t_block);
+
+    tiny_node = tiny_head;
+    while (tiny_node->next != NULL)
+    {
+        if ((char *)tiny_node->next - (char *)tiny_node->blck_limit >= size)
+	    break;
+        tiny_node = tiny_node->next;
+    }
+
+    if ((char*)tiny_node->blck_limit + size > (char*)tiny_head + max)
+    {
+        errno = ENOMEM;
+        return (NULL);
+    }
+    ADD_CHUNK_TO_DMA(TINY, tiny_node, tmp);
+    return (tiny_node->next + 1);
+}
+
 
 static void init_tiny(void)
 {
     g_dma->tiny = (struct t_block *)(g_dma + 1);
     g_dma->tiny->blck_limit = g_dma->tiny + 2;
-    g_dma->tiny_limit = T_LIMIT;
+    g_dma->tiny_limit = (size_t)T_LIMIT;
 
     g_dma->tiny->next = NULL;
     g_dma->tiny->size = 0;
@@ -22,7 +60,7 @@ static void init_small(void)
 {
     g_dma->small = (struct s_block *)((char *)g_dma->tiny + g_dma->tiny_limit);
     g_dma->small->blck_limit = g_dma->small + 1;
-    g_dma->small_limit = S_LIMIT;
+    g_dma->small_limit = (size_t)S_LIMIT;
 
     g_dma->small->next = NULL;
     g_dma->small->size = 0;
@@ -31,11 +69,37 @@ static void init_small(void)
     g_dma->addr.small_start = (char*)g_dma->small;
 }
 
+long print_alloc(struct t_block *node)
+{
+  long size;
 
+  printf("%lx", (long)(node + 1));
+  printf(" - ");
+  printf("%lx", (long)(node->blck_limit));
+  printf(" : ");
+  size = node->size;//(char*)node->blck_limit - (char*)node - sizeof(struct t_block);
+  printf("%ld ", size);
+  puts(" octets\n");
+  return (size);
+}
+
+long print_malloc(struct t_block *node)
+{
+  long total;
+
+  total = 0;
+  while (node->next != NULL)
+    {
+      node = node->next;
+      total += print_alloc(node);
+    }
+    return (total);
+}
 
 void show_alloc_mem(void)
 {
     printf("TINY: %p\n", g_dma->tiny);
+    print_malloc(g_dma->tiny);
     printf("SMALL: %p\n", g_dma->small);
 }
 
@@ -49,22 +113,46 @@ void show_alloc_mem(void)
 
 void init_memory(void)
 {
-	const size_t SIZE = (sizeof(struct ovrl_block) + T_PAGE_SIZE + S_PAGE_SIZE) * 200;
   	if (!g_dma)
 	{
-		if ((g_dma = mmap(NULL, SIZE, MMAP_FLAGS, OFFSET, 0)) == MAP_FAILED)
-			exit(0);
-		init_tiny();
-        init_small();
-    }
+	  if ((g_dma = mmap(NULL, HDR_SIZE
+                              + PREALOC_SIZE_TINY
+                              + PREALOC_SIZE_SMALL,
+                              MMAP_FLAGS, OFFSET, 0)) == MAP_FAILED)
+	    	exit(0);
+	  	init_tiny();
+		init_small();
+		g_dma->get_tiny = add_tiny;
+        //g_dma->get_large = add_large;
+       // g_dma->get_small = get_small;
+	} 
 	  //init_large();
 }
 
+void *mallok(size_t size)
+{
+    pthread_mutex_lock(&mutex);
+
+    if (g_dma == NULL)
+        init_memory();
+
+    if (size <= T_PAGE_SIZE)
+        return (g_dma->get_tiny(g_dma->tiny, size, g_dma->tiny_limit));
+  /*else if (size < S_LIMIT)
+    return (g_dma->get_small(g_dma->small, size, g_dma->small_limit));
+  else
+  return (g_dma->get_large(g_dma->large, size));*/
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
 
 int main(void)
 {
-    init_memory();
-    show_alloc_mem();
-
-    return 0;
+  //   init_memory();
+  char *ptr = mallok(20);
+  mallok(21);
+  mallok(24);
+  mallok(23);
+  show_alloc_mem();
+  return 0;
 }
